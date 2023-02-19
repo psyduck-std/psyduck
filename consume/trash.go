81bb91b7024724766ea740e497d0cab169c53cfb
Eva <mail@gastrodon.io>
2023-02-19 13:22:16 -0500

    use done channel
    don't buffer trash data thats stupid

--- consume/trash.go
+++ consume/trash.go
@@ -5,14 +5,17 @@ import (
 )
 
 func Trash(_ sdk.Parser, _ sdk.SpecParser) (sdk.Consumer, error) {
-	return func() (chan []byte, chan error) {
-		data := make(chan []byte, 32)
+	return func() (chan []byte, chan error, chan bool) {
+		data := make(chan []byte)
+		done := make(chan bool)
 
 		go func() {
 			for range data {
 			}
+
+			done <- true
 		}()
 
-		return data, nil
+		return data, nil, done
 	}, nil
 }
